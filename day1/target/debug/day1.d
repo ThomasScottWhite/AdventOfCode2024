4f46745000c87175af692902369a2f77378b2dab
Thomas <tsw96d@umsystem.edu>
2024-12-05 05:16:02 -0600

    by by

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/thomas/AdventOfCode2024/day1/target/debug/day1: /home/thomas/AdventOfCode2024/day1/src/main.rs
--- day1/target/debug/deps/day1-754d9a61e80a487e.d
+++
@@ -1,5 +0,0 @@
-/home/thomas/AdventOfCode2024/day1/target/debug/deps/day1-754d9a61e80a487e: src/main.rs
-
-/home/thomas/AdventOfCode2024/day1/target/debug/deps/day1-754d9a61e80a487e.d: src/main.rs
-
-src/main.rs:
